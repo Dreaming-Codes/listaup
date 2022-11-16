1271f61fa7595053da51f0cc92127633af83d2f9
DreamingCodes <dev@dreaming.codes>
2022-11-17 00:11:29 +0100

    Added hack for older devices

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import { ResizeObserver } from "resize-observer";
 
 import './styles/common.scss'
 import './styles/fonts.scss'
 import './styles/transitions.scss'
 import './styles/scrollbar.scss'
+
+if(window.ResizeObserver === undefined) {
+    // @ts-ignore
+    window.ResizeObserver = ResizeObserver;
+}
 
 const app = createApp(App)
 
